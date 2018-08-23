fb6f894efa4c9790848d6077943b9efbb4c38404
rote66 <pontifex23872388@gmail.com>
2018-08-23 19:57:10 +0800

    fix some names

--- omni_eve.mk
+++ omni_eve.mk
@@ -1,15 +1,12 @@
 # Release name
-PRODUCT_RELEASE_NAME := eve
+PRODUCT_RELEASE_NAME := SG507SH
 
-LOCAL_PATH := device/SHARP/eve
+LOCAL_PATH := device/SHARP/eve_sprout
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := eve
 
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
@@ -17,13 +14,13 @@ TARGET_SCREEN_WIDTH := 1080
 include $(call all-subdir-makefiles)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := eve
-PRODUCT_NAME := omni_eve
-PRODUCT_BRAND := SHARP
+PRODUCT_DEVICE := eve_sprout
+PRODUCT_NAME := omni_eve_sprout
+PRODUCT_BRAND := SG
 PRODUCT_MODEL := 507SH
 PRODUCT_MANUFACTURER := SHARP
 
 # enable stock zip packages flash
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.build.product=507SH \
-    ro.product.device=SG507SH
+    ro.product.device=eve_sprout
